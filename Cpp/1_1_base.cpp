d49b21e65e4aeb30d74489bca8549dc936ff29af
Lacrima98654 <romeokakpo3@gmail.com>
2023-02-02 11:48:59 +0100

    Bases

---
+++ Cpp/1_1_base.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	// Langage C++11
+	// Some code here
+}
---
+++ Cpp/1_2_io.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  // Mettre ça toujours en début de code
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int x;
+
+  // Utiliser \n au lieu de endl
+
+  // Il est plus efficace d'utiliser scanf et printf que d'utiliser cin et cout
+  // Mais ces deux sont plus difficile à utiliser
+
+  // Lecture d'une chaîne au clavier
+  string s;
+  getline(cin, s);
+
+  // Pour lire des valeurs à l'écran de manière infini
+  while (cin >> x)
+  {
+  }
+
+  // Pour utiliser des fichiers tout en utilisant toujours les entrées sorties standard
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+}
